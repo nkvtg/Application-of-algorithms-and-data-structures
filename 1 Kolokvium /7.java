6fd627bf3e64d0672ecfdf606c5e95c5e2c6eb1f
Tome <149436522+tgxfc@users.noreply.github.com>
2024-11-03 19:48:55 +0100

    Create 7.java

---
+++ 1 Kolokvium /7.java
@@ -0,0 +1,244 @@
+//За дадена двојно поврзана листа од N цели броеви, треба да се најде бројот на елементи такви што просекот на елементите од пред него во листата е поголем од просекот на елементи после него во листата.
+
+//Влез: Првиот број од влезот е бројот на елементи во листата N, а потоа во следниот ред се дадени самите елементи одделени со празно место.
+
+//Излез: Бројот на елементи што го задоволуваат условот.
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        DLL<Integer> dll = new DLL<>();
+
+        for (int i = 0; i < n; i++) {
+            dll.insertLast(sc.nextInt());
+        }
+
+        int c = dll.count();
+        System.out.println(c);
+    }
+}
+
+
+class DLLNode<E> {
+    protected E element;
+    protected DLLNode<E> pred, succ;
+    public DLLNode(E elem, DLLNode<E> pred, DLLNode<E> succ) {
+        this.element = elem;
+        this.pred = pred;
+        this.succ = succ;
+    }
+
+    @Override
+    public String toString() {
+        return element.toString();
+    }
+}
+
+class DLL<E> {
+    private DLLNode<E> first, last;
+
+    public DLL() {
+        // Construct an empty SLL
+        this.first = null;
+        this.last = null;
+    }
+
+    public void insertFirst(E o) {
+        DLLNode<E> ins = new DLLNode<E>(o, null, first);
+        if (first == null)
+            last = ins;
+        else
+            first.pred = ins;
+        first = ins;
+    }
+
+    public void insertLast(E o) {
+        if (first == null)
+            insertFirst(o);
+        else {
+            DLLNode<E> ins = new DLLNode<E>(o, last, null);
+            last.succ = ins;
+            last = ins;
+        }
+    }
+
+    public void insertAfter(E o, DLLNode<E> after) {
+        if (after == last) {
+            insertLast(o);
+            return;
+        }
+        DLLNode<E> ins = new DLLNode<E>(o, after, after.succ);
+        after.succ.pred = ins;
+        after.succ = ins;
+    }
+
+    public void insertBefore(E o, DLLNode<E> before) {
+        if (before == first) {
+            insertFirst(o);
+            return;
+        }
+        DLLNode<E> ins = new DLLNode<E>(o, before.pred, before);
+        before.pred.succ = ins;
+        before.pred = ins;
+    }
+
+    public E deleteFirst() {
+        if (first != null) {
+            DLLNode<E> tmp = first;
+            first = first.succ;
+            if (first != null) first.pred = null;
+            if (first == null)
+                last = null;
+            return tmp.element;
+        } else
+            return null;
+    }
+
+    public E deleteLast() {
+        if (first != null) {
+            if (first.succ == null)
+                return deleteFirst();
+            else {
+                DLLNode<E> tmp = last;
+                last = last.pred;
+                last.succ = null;
+                return tmp.element;
+            }
+        } else
+            return null;
+    }
+
+    public E delete(DLLNode<E> node) {
+        if (node == first) {
+            return deleteFirst();
+        }
+        if (node == last) {
+            return deleteLast();
+        }
+        node.pred.succ = node.succ;
+        node.succ.pred = node.pred;
+        return node.element;
+
+    }
+
+    public DLLNode<E> find(E o) {
+        if (first != null) {
+            DLLNode<E> tmp = first;
+            while (!tmp.element.equals(o) && tmp.succ != null)
+                tmp = tmp.succ;
+            if (tmp.element.equals(o)) {
+                return tmp;
+            } else {
+                System.out.println("Elementot ne postoi vo listata");
+            }
+        } else {
+            System.out.println("Listata e prazna");
+        }
+        return null;
+    }
+
+    public void deleteList() {
+        first = null;
+        last = null;
+    }
+
+    public int getSize() {
+        int listSize = 0;
+        DLLNode<E> tmp = first;
+        while(tmp != null) {
+            listSize++;
+            tmp = tmp.succ;
+        }
+        return listSize;
+    }
+
+    @Override
+    public String toString() {
+        String ret = new String();
+        if (first != null) {
+            DLLNode<E> tmp = first;
+            ret += tmp.toString();
+            while (tmp.succ != null) {
+                tmp = tmp.succ;
+                ret += "<->" + tmp.toString();
+            }
+        } else
+            ret = "Prazna lista!!!";
+        return ret;
+    }
+
+    public String toStringR() {
+        String ret = new String();
+        if (last != null) {
+            DLLNode<E> tmp = last;
+            ret += tmp.toString();
+            while (tmp.pred != null) {
+                tmp = tmp.pred;
+                ret += "<->" + tmp.toString();
+            }
+        } else
+            ret = "Prazna lista!!!";
+        return ret;
+    }
+
+    public DLLNode<E> getFirst() {
+        return first;
+    }
+
+    public DLLNode<E> getLast() {
+
+        return last;
+    }
+
+    public void mirror() {
+
+        DLLNode<E> tmp = null;
+        DLLNode<E> current = first;
+        last = first;
+        while(current!=null) {
+            tmp = current.pred;
+            current.pred = current.succ;
+            current.succ = tmp;
+            current = current.pred;
+        }
+
+        if(tmp!=null && tmp.pred!=null) {
+            first=tmp.pred;
+        }
+    }
+
+    public int count() {
+        DLLNode<E> curr = first;
+        int count = 0;
+        while(curr!=null) {
+            DLLNode<E> left = curr.pred;
+            DLLNode<E> right = curr.succ;
+            float leftC = 0, rightC = 0;
+            int leftN = 0, rightN = 0;
+            while(left != null) {
+                leftC += (Integer)left.element;
+                leftN++;
+                left = left.pred;
+            }
+            leftC /= leftN;
+            while(right != null) {
+                rightC += (Integer)right.element;
+                rightN++;
+                right = right.succ;
+            }
+            rightC /= rightN;
+
+            if(leftC>rightC){
+                count++;
+            }
+            curr = curr.succ;
+        }
+
+        return count;
+    }
+}
+
